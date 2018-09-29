68b2b02faf030d017ada0d57cf999be24c299dd6
yanhao.xu <43689490+YanhaoXu@users.noreply.github.com>
2018-09-29 10:50:59 +0800

    修改Java文件
    练习

--- Hello.java
+++ Hello.java
@@ -1,1 +1,11 @@
+package com.yanhao.java8;
 
+/**
+ * @author yanhao.xu
+ * @Create 2018-09-29 10:49
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
